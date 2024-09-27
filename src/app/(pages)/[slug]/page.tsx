c3423298a700a1a27a6b6e65a3a6c31954eb4c9e
Andrew-Tsegaye <andrewtsegaye7@gmail.com>
2024-09-27 21:46:29 +0300

    implement promo section

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -22,6 +22,7 @@ export const dynamic = 'force-dynamic'
 
 import classes from './index.module.scss'
 import Categories from '../../_components/Categories'
+import Promotion from '../../_components/Promotion'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
@@ -65,6 +66,7 @@ export default async function Page({ params: { slug = 'home' } }) {
 
           <Gutter className={classes.home}>
             <Categories categories={categories} />
+            <Promotion />
           </Gutter>
         </section>
       ) : (
