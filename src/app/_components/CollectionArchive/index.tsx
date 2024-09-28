aab58d39e06502e44a51c184ffeaf8e83826c359
Andrew-Tsegaye <andrewtsegaye7@gmail.com>
2024-09-28 11:05:48 +0300

    develop fiters

--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -11,6 +11,7 @@ import { PageRange } from '../PageRange'
 import { Pagination } from '../Pagination'
 
 import classes from './index.module.scss'
+import { useFilter } from '../../_providers/Filter'
 
 type Result = {
   docs: (Product | string)[]
@@ -38,6 +39,7 @@ export type Props = {
 }
 
 export const CollectionArchive: React.FC<Props> = props => {
+  const { categoryFilters, sort } = useFilter()
   const {
     categories: catsFromProps,
     className,
@@ -49,7 +51,6 @@ export const CollectionArchive: React.FC<Props> = props => {
     relationTo,
     selectedDocs,
     showPageRange,
-    sort = '-createdAt',
   } = props
 
   const [results, setResults] = useState<Result>({
@@ -82,9 +83,9 @@ export const CollectionArchive: React.FC<Props> = props => {
   const scrollToRef = useCallback(() => {
     const { current } = scrollRef
     if (current) {
-      // current.scrollIntoView({
-      //   behavior: 'smooth',
-      // })
+      current.scrollIntoView({
+        behavior: 'smooth',
+      })
     }
   }, [])
 
@@ -116,10 +117,13 @@ export const CollectionArchive: React.FC<Props> = props => {
           page,
           sort,
           where: {
-            ...(categories
+            ...(categoryFilters && categoryFilters?.length > 0
               ? {
                   categories: {
-                    in: categories,
+                    in:
+                      typeof categoryFilters === 'string'
+                        ? [categoryFilters]
+                        : categoryFilters.map((cat: string) => cat).join(','),
                   },
                 }
               : {}),
@@ -162,7 +166,7 @@ export const CollectionArchive: React.FC<Props> = props => {
     return () => {
       if (timer) clearTimeout(timer)
     }
-  }, [page, categories, relationTo, onResultChange, sort, limit, populateBy])
+  }, [page, categories, relationTo, onResultChange, sort, limit, populateBy, categoryFilters])
 
   return (
     <div className={[classes.collectionArchive, className].filter(Boolean).join(' ')}>
