dc212128c88d5f17e07622e33ff054371bc3d52e
Andrew-Tsegaye <andrewtsegaye7@gmail.com>
2024-09-28 17:37:10 +0300

    implemnt Related Products

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -1,26 +1,17 @@
 import React, { Fragment } from 'react'
-import Link from 'next/link'
 
-import { Product } from '../../../payload/payload-types'
+import { Category, Product } from '../../../payload/payload-types'
 import { AddToCartButton } from '../../_components/AddToCartButton'
 import { Gutter } from '../../_components/Gutter'
 import { Media } from '../../_components/Media'
-import { Message } from '../../_components/Message'
 import { Price } from '../../_components/Price'
-import RichText from '../../_components/RichText'
 
 import classes from './index.module.scss'
 
 export const ProductHero: React.FC<{
   product: Product
 }> = ({ product }) => {
-  const {
-    id,
-    stripeProductID,
-    title,
-    categories,
-    meta: { image: metaImage, description } = {},
-  } = product
+  const { title, categories, meta: { image: metaImage, description } = {} } = product
 
   return (
     <Gutter className={classes.productHero}>
@@ -37,34 +28,31 @@ export const ProductHero: React.FC<{
         <div className={classes.categoryWrapper}>
           <div className={classes.categories}>
             {categories?.map((category, index) => {
-              if (typeof category === 'object' && category !== null) {
-                const { title: categoryTitle } = category
+              const { title: categoryTitle } = category as Category
 
-                const titleToUse = categoryTitle || 'Untitled category'
+              const titleToUse = categoryTitle || 'Generic'
+              const isLast = index === categories.length - 1
 
-                const isLast = index === categories.length - 1
-
-                return (
-                  <p key={index} className={classes.category}>
-                    {titleToUse}
-                    {!isLast && <Fragment>, &nbsp;</Fragment>}{' '}
-                    <span className={classes.separator}>|</span>
-                  </p>
-                )
-              }
+              return (
+                <p key={index} className={classes.category}>
+                  {titleToUse} {!isLast && <Fragment>, &nbsp;</Fragment>}
+                  <span className={classes.separator}>|</span>
+                </p>
+              )
             })}
           </div>
-          <p className={classes.stock}> In stock </p>
+          <p className={classes.stock}> In stock</p>
         </div>
-      </div>
 
-      <Price product={product} button={false} />
+        <Price product={product} button={false} />
 
-      <div className={classes.description}>
-        <h6>Description</h6>
-        <p>{description}</p>
+        <div className={classes.description}>
+          <h6>Description</h6>
+          <p>{description}</p>
+        </div>
+
+        <AddToCartButton product={product} className={classes.addToCartButton} />
       </div>
-      <AddToCartButton product={product} className={classes.addToCartButton} />
     </Gutter>
   )
 }
